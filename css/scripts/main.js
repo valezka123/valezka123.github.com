dba2d564356f53378d521d6df9d59b0d64bd7206
valezka123 <42702595+valezka123@users.noreply.github.com>
2018-09-23 02:54:22 -0300

    inicial

---
+++ css/scripts/main.js
@@ -0,0 +1,21 @@
+//alert("Hola JS");
+//console.log("hola consola");
+//DOM (document ibject model)
+
+//let element= document.queryselector(",fa.bomb");
+//los obejtos dentro del DOM se conocen como nodos
+
+//let elementlist = document.querySelectorAll(".fa-angry");
+
+
+// elementlist.forEach(function(element){
+//     element.addEventListener('click' ,function(){
+//        alert("Bomba Clickeada");
+//        element.classList.remove("far");
+//        element.classList.add("fas");
+    
+//     })
+
+// })
+
+//evento
